f2bb10ed941e5b14e1139f9eeec1627ee67b9411
whelmer <whelmer_neto@Hotmail.com>
2021-06-14 11:15:39 -0300

    adding credit.c

---
+++ credit.c
@@ -0,0 +1,68 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+//Ask for credit card number
+
+int main()
+{
+   long credit;
+   long credit_copy;
+   int total_sum = 0, pos = 0, total_length = 0;
+
+   do
+   {
+    credit = get_long("Credit card number: \n");
+    credit_copy = credit;
+
+// Checks if a credit card number was inserted 
+    
+    while (credit != 0)
+        {
+
+            if (pos % 2 != 0) {
+                int temp = 2 * (credit % 10);
+                if (temp > 9) {
+                    total_sum = total_sum + temp / 10 + temp % 10;
+                } else {
+                    total_sum = total_sum + temp;
+                }
+            } else {
+                total_sum = total_sum + (credit % 10);
+            }
+
+            credit = credit / 10;
+            pos = pos + 1;
+            total_length = total_length + 1;
+        }
+   }
+
+    while (credit != 0);
+
+    if (total_sum % 10 == 0) {
+        long amex_start = credit_copy / 10000000000000;
+        if ((amex_start == 34 || amex_start == 37) && total_length == 15) {
+            printf("AMEX\n");
+            return 0;
+        }
+
+    long master_card_start = credit_copy / 100000000000000;
+
+    if (( total_length == 16) && (master_card_start >= 51 && master_card_start <= 55)) {
+        printf("MASTERCARD\n");
+        return 0;
+    }
+
+    long visa_start = credit_copy / 1000000000000;
+    if ((total_length == 16 || total_length == 13) && (visa_start == 4 || master_card_start / 10 == 4 )) {
+        printf("VISA\n");
+       return 0;
+    }
+
+    printf ("INVALID\n");
+
+    } else {
+        printf("INVALID\n");
+    }
+
+}
